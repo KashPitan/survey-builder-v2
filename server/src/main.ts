02b9961e3e47ff9bb444a3c60383f34bfba3b7e6
Kash Pitan <kashpitan@googlemail.com>
2023-06-25 19:24:48 +0100

    feat: production port settings

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+
+  const port =
+    process.env.NODE_ENV === 'production'
+      ? `0.0.0.0:${process.env.PORT}`
+      : 4000;
+  console.log(port);
+  await app.listen(port);
 }
 
 bootstrap();
