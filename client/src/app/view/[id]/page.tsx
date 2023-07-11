f24067a83fd4aace395131444d9468910398b705
Kash Pitan <kashpitan@googlemail.com>
2023-07-11 16:07:01 +0100

    chore: remove commented code

--- client/src/app/view/[id]/page.tsx
+++ client/src/app/view/[id]/page.tsx
@@ -1,13 +1,10 @@
 // TODO: added this to prevent use context error from styled components: needs to be fixed
 
 import Form from './components/Form';
-import FormContainer from './components/FormContainer';
-import SurveyBuilder from './components/SurveyBuilder';
 
 async function getSurvey(id: number) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/surveys/${id}`);
   const data = await res.json();
-  // console.log(data);
   return data;
 }
 
@@ -24,19 +21,6 @@ const page: ({
     <>
       <h1>Survey: {survey.name}</h1>
       <Form questions={questions} />
-      {/* <form onSubmit={handleSubmit}>
-        <SurveyBuilder questions={questions} />{' '}
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Submit
-        </button>
-      </form> */}
-
-      {/* <FormContainer questionCount={questions.length}>
-        <SurveyBuilder questions={questions} />
-      </FormContainer> */}
     </>
   );
 };
